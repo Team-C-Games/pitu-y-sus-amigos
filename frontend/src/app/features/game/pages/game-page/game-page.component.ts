99c34fcac56e8a9e3e38b409ce2f5c792071782c
Virginia Torena <pitutorena@gmail.com>
2026-07-22 19:06:35 -0300

    ajustes de diseño

--- frontend/src/app/features/game/pages/game-page/game-page.component.ts
+++ frontend/src/app/features/game/pages/game-page/game-page.component.ts
@@ -4,12 +4,23 @@ import { GameBoardComponent } from '../../components/game-board/game-board.compo
 import { SidePanelComponent } from '../../components/side-panel/side-panel.component';
 import { ActionPanelComponent } from '../../components/action-panel/action-panel.component';
 import { GameUiFacade } from '../../services';
+import { PlayerStatusComponent } from '../../components/player-status/player-status.component';
+import { TreasurePanelComponent } from '../../hud/treasure-panel/treasure-panel.component';
+import { DicePanelComponent } from '../../hud/dice-panel/dice-panel.component';
 
 @Component({
   selector: 'app-game-page',
   templateUrl: './game-page.component.html',
   styleUrls: ['./game-page.component.scss'],
-  imports: [CommonModule, GameBoardComponent, SidePanelComponent, ActionPanelComponent],
+  imports: [
+    CommonModule, 
+    GameBoardComponent, 
+    SidePanelComponent, 
+    ActionPanelComponent,
+    PlayerStatusComponent,
+    TreasurePanelComponent,
+    DicePanelComponent
+  ],
 })
 export class GamePageComponent {
   private readonly gameUiFacade = inject(GameUiFacade);
