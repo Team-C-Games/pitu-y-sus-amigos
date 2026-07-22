493583d70c1143fe02f4dbb82e7f21bbe2addc39
Guillermojuan <128424811+Guillermojuan@users.noreply.github.com>
2026-07-22 15:45:28 -0300

    Parametrizar environments y CORS para producción

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  hubUrl: 'https://pitu-y-sus-amigos-production.up.railway.app/hubs/game',
+};
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  hubUrl: 'http://localhost:5291/hubs/game',
+};
